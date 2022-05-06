e96b9931611c20d0f89b0b34af879e5670556db5
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-05-06 20:29:53 +0300

    .

---
+++ HotelFinder.DataAccess/Concrete/HotelRepository.cs
@@ -0,0 +1,54 @@
+﻿using HotelFinder.DataAccess.Abstract;
+using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess.Concrete
+{
+    public class HotelRepository : IHotelRepository
+    {
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            using (var hotelDbContext = new HotelDbContext())
+            {
+                hotelDbContext.Hotels.Add(hotel);
+                hotelDbContext.SaveChanges();
+                return hotel;
+            }
+        }
+
+        public void DeleteHotel(int id)
+        {
+            using (var hotelDbContext = new HotelDbContext())
+            {
+                var deletedHotel = GetHotelById(id);
+                hotelDbContext.Hotels.Remove(deletedHotel);
+                hotelDbContext.SaveChanges();
+            }
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            using (var hotelDbContext = new HotelDbContext())
+            {
+                return hotelDbContext.Hotels.Find(id);
+            }
+        }
+
+        public List<Hotel> GetHotels()
+        {
+            using (var hotelDbContext = new HotelDbContext())
+            {
+                return hotelDbContext.Hotels.ToList();
+            }
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-3CLC889; Database=HotelDb; uid=sa; pwd=123;");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
